44a2fdf78c6d8c479b0f0ff06cb4221169e9a576
Andrea McIntosh <akmcinto@ualberta.ca>
2016-01-20 10:16:41 -0700

    continuing

--- MatrixThreads.c
+++ MatrixThreads.c
@@ -6,6 +6,7 @@ int Lab1_loadinput(int ***A, int ***B, int *n);
 int Lab1_saveoutput(int **C, int *n, double Time);
 void *pCalc(void* arg_p);
 
+// Struct to pass info into threads
 typedef struct ThrDt {
     int **matA, **matB, **matC;
     int n;
@@ -15,13 +16,16 @@ typedef struct ThrDt {
 int main (int argc, char* argv[])
 {
     int n, p; // matrix size, number of threads
-    int i, j, k, thread;
+    int i, j, k, thread; // counters
     FILE* fp;
-    int **A; int **B; int **C;
+    int **A; int **B; int **C; // matrices
 
+    // Get number of threads
     p = atoi(argv[1]);
+    // Load matrices and size from file
     Lab1_loadinput(&A, &B, &n);
 
+    // Allocating output matrix and setting elements to 0
     C = malloc(n * sizeof(int*));
     for (i = 0; i < n; i++) {
         C[i] = malloc(n * sizeof(int));
@@ -32,10 +36,12 @@ int main (int argc, char* argv[])
       }
     }
 
+    // Allocating threads and their structs containing the data
     pthread_t *thread_handles = malloc(p * sizeof(pthread_t));
     threadData *thread_data = malloc(p * sizeof(threadData));
 
     for (thread = 0; thread < p; thread++) {
+      // Give each the data they need
       thread_data[thread].matA = A;
       thread_data[thread].matB = B;
       thread_data[thread].matC = C;
@@ -47,12 +53,15 @@ int main (int argc, char* argv[])
              pCalc, (void*) &thread_data[thread]);
     }
 
+    // Wait for threads to complete
     for (thread = 0; thread < p; thread++) {
       pthread_join(thread_handles[thread], NULL);
     }
 
+    // Save to data_output file
     Lab1_saveoutput(C, &n, 0);
 
+    // Free allocated memory
     free(thread_data);
     free(thread_handles);
     free(A); free(B); free(C);
@@ -65,12 +74,17 @@ void *pCalc(void* arg_p) {
     int i, j;
     int sum = 0;
 
+    // n^2/p gives the number of elements in the matrix each thread is responisble for
     int fac = (td->n)*(td->n)/(td->p);
-    for (i = td->rank * fac; i < (td->rank + 1) * fac; i++) {
+
+    for (i = td->rank * fac; i < (td->rank + 1) * fac; i++) { // gives element number (0-n)
         for(j = 0; j < td->n; j++) {
             // Scan matrix A along rows, matrix B along columns
+            // i is floored (integer division) to give row number corresponding to matrix element i
+            // i mod n gives the column number corresponding to matrix element i
             sum += (td->matA)[i / (td->n)][j] * (td->matB)[j][i % (td->n)];
         }
+        // Add temporary sum to corresponding matrix element
         (td->matC)[i / (td->n)][i % (td->n)] += sum;
         sum = 0;
     }
